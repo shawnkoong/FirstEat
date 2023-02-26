91255a020f915bdf3fc42a5a41e6a01aef931afe
Shawn Koong <shawnkoong@gmail.com>
2023-02-25 19:52:40 -0800

    first commit

---
+++ backend/src/main/java/com/firsteat/firsteat/FirsteatApplication.java
@@ -0,0 +1,13 @@
+package com.firsteat.firsteat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsteatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsteatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/firsteat/firsteat/FirsteatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firsteat.firsteat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsteatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
