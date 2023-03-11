8f2e40c4c9447c128dd05148e0de8707a5104513
Shawn Koong <shawnkoong@gmail.com>
2023-03-11 00:00:10 -0800

    profile icon dropdown

--- frontend/src/components/ProfileDropdown/ProfileDropdown.js
+++ frontend/src/components/ProfileDropdown/ProfileDropdown.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { Typography, Box, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/userReducer";
+import { logoutToken } from "../../store/authReducer"
 
 const ProfileDropdown = () => {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutToken);
+    dispatch(logout);
+    navigate("/");
+  }
+
   return (
     <Box
       sx={{
@@ -26,12 +40,17 @@ const ProfileDropdown = () => {
               Orders
             </Typography>
           </Grid>
+          <Grid item onClick={navigate("/map")}>
+            <Typography gutterBottom variant="subtitle1" color="primary">
+              Map
+            </Typography>
+          </Grid>
           <Grid item>
             <Typography gutterBottom variant="subtitle1" color="primary">
               Favorites
             </Typography>
           </Grid>
-          <Grid item>
+          <Grid item onClick={handleLogout}>
             <Typography gutterBottom variant="subtitle1" color="error">
               Logout
             </Typography>
