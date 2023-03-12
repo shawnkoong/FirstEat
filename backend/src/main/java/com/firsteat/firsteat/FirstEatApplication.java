495dbb72c1b8d32bfc46920e407105435a90a5fa
Shawn Koong <shawnkoong@gmail.com>
2023-03-11 17:07:01 -0800

    renaming

--- backend/src/main/java/com/firsteat/firsteat/FirstEatApplication.java
+++ backend/src/main/java/com/firsteat/firsteat/FirstEatApplication.java
@@ -8,10 +8,10 @@ import com.firsteat.firsteat.configuration.RsaKeyProperties;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
-public class FirsteatApplication {
+public class FirstEatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirsteatApplication.class, args);
+		SpringApplication.run(FirstEatApplication.class, args);
 	}
 
 }
