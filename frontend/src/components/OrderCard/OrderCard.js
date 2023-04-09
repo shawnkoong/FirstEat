c0d710de17b3a7a1ad7c04004d11ef0b030b2924
Shawn Koong <shawnkoong@gmail.com>
2023-04-08 23:36:00 -0700

    order card

--- frontend/src/components/OrderCard/OrderCard.js
+++ frontend/src/components/OrderCard/OrderCard.js
@@ -1,15 +1,44 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Divider, Paper, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const OrderCard = ({ order }) => {
+  const timestamp = new Date(order.timestamp);
+  console.log(order);
+  const localTime = `${
+    timestamp.getMonth() + 1
+  }/${timestamp.getDate()}/${timestamp.getFullYear()} ${
+    timestamp.getHours() % 12 == 0 ? 12 : timestamp.getHours() % 12
+  }:${timestamp.getMinutes()} ${timestamp.getHours() >= 12 ? "PM" : "AM"}`;
   return (
     <Paper elevation={2}>
       <Box display="flex" width="100%">
-        <Box display="flex" flexDirection="column" width="70%">
-          <Link to={`/restaurant/${order.restaurantId}`} style={{ textDecoration: "none", color: "blue" }}>
-            <Typography variant="h6">{order.restaurantName}</Typography>
-          </Link>
+        <Box display="flex" flexDirection="column" width="70%" mx={2}>
+          <Box>
+            <Box display="flex" justifyContent="space-between">
+              <Link
+                to={`/restaurant/${order.restaurantId}`}
+                style={{ textDecoration: "none", color: "blue" }}
+              >
+                <Typography variant="h6">{order.restaurantName}</Typography>
+              </Link>
+              <Typography>${(order.totalPrice / 100).toFixed(2)}</Typography>
+            </Box>
+            <Typography variant="subtitle2" sx={{ color: "text.secondary" }}>
+              Ordered on {localTime}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box>
+            {order.quantityOrdered.map((quantity, i) => (
+              <>
+                <Typography key={i}>
+                  {quantity} x {order.itemsOrdered[i].name}
+                </Typography>
+                <Divider />
+              </>
+            ))}
+          </Box>
         </Box>
         <Box display="flex" width="30%">
           <div>box 2</div>
--- frontend/src/components/ProfileDropdown/ProfileDropdown.js
+++ frontend/src/components/ProfileDropdown/ProfileDropdown.js
@@ -3,10 +3,9 @@ import { Typography, Box, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/userReducer";
-import { logoutToken } from "../../store/authReducer"
+import { logoutToken } from "../../store/authReducer";
 
 const ProfileDropdown = () => {
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -14,7 +13,7 @@ const ProfileDropdown = () => {
     dispatch(logoutToken());
     dispatch(logout());
     navigate("/");
-  }
+  };
 
   return (
     <Box
@@ -26,35 +25,61 @@ const ProfileDropdown = () => {
         padding: "20px",
         bgcolor: "white",
         boxShadow: "0px 0px 7px -5px rgba(0, 0, 0, 0.5)",
+        py: 1,
       }}
     >
-        <Grid container direction="column" alignContent="space-around">
-          <Grid item>
-            <Typography gutterBottom variant="subtitle1" color="primary" sx={{"&:hover": { cursor: "pointer" }}}>
-              Profile
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="subtitle1" color="primary" sx={{"&:hover": { cursor: "pointer" }}}>
-              Orders
-            </Typography>
-          </Grid>
-          <Grid item onClick={navigate("/map")}>
-            <Typography gutterBottom variant="subtitle1" color="primary" sx={{"&:hover": { cursor: "pointer" }}}>
-              Map
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="subtitle1" color="primary" sx={{"&:hover": { cursor: "pointer" }}}>
-              Favorites
-            </Typography>
-          </Grid>
-          <Grid item onClick={handleLogout} >
-            <Typography gutterBottom variant="subtitle1" color="error" sx={{"&:hover": { cursor: "pointer" }}}>
-              Logout
-            </Typography>
-          </Grid>
+      <Grid container direction="column" alignContent="space-around">
+        <Grid item onClick={() => navigate("/profile")}>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            color="primary"
+            sx={{ "&:hover": { cursor: "pointer" } }}
+          >
+            Profile
+          </Typography>
         </Grid>
+        <Grid item onClick={() => navigate("/orders")}>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            color="primary"
+            sx={{ "&:hover": { cursor: "pointer" } }}
+          >
+            Orders
+          </Typography>
+        </Grid>
+        <Grid item onClick={() => navigate("/map")}>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            color="primary"
+            sx={{ "&:hover": { cursor: "pointer" } }}
+          >
+            Map
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            color="primary"
+            sx={{ "&:hover": { cursor: "pointer" } }}
+          >
+            Favorites
+          </Typography>
+        </Grid>
+        <Grid item onClick={handleLogout}>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            color="error"
+            sx={{ "&:hover": { cursor: "pointer" } }}
+          >
+            Logout
+          </Typography>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
